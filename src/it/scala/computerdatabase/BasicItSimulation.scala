3d9dc954f57f585dede73496dbe92a4d109dbbeb
George Leung <phisgr@gmail.com>
2019-06-25 16:51:45 +0800

    a more interesting test

--- src/it/scala/computerdatabase/BasicItSimulation.scala
+++ src/it/scala/computerdatabase/BasicItSimulation.scala
@@ -2,6 +2,7 @@ package computerdatabase
 
 import com.github.phisgr.gatling.grpc.Predef._
 import io.gatling.core.Predef._
+import io.grpc.health.v1.health.HealthCheckResponse.ServingStatus.SERVING
 import io.grpc.health.v1.health.{HealthCheckRequest, HealthGrpc}
 
 import scala.concurrent.duration._
@@ -9,13 +10,17 @@ import scala.concurrent.duration._
 class BasicItSimulation extends Simulation {
 
   val grpcConf = grpc(managedChannelBuilder("photoslibrary.googleapis.com", 443))
+  val request = grpc("request_1")
+    .rpc(HealthGrpc.METHOD_CHECK)
+    .payload(HealthCheckRequest.defaultInstance)
+    .extract(_.status.some)(_ is SERVING)
+
   val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(
-      grpc("request_1")
-        .rpc(HealthGrpc.METHOD_CHECK)
-        .payload(HealthCheckRequest.defaultInstance)
-    )
+    .exec(request)
     .pause(7.seconds)
+    .exec(request)
+    .exec(request)
+    .exec(request)
 
   setUp(scn.inject(atOnceUsers(1)).protocols(grpcConf))
 }
