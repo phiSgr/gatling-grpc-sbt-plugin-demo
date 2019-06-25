ac1ae8e07db0adc355f3de33d859caa40647bfb0
George Leung <phisgr@gmail.com>
2019-06-25 16:44:13 +0800

    gRPC test in src/it

--- src/it/scala/computerdatabase/BasicItSimulation.scala
+++ src/it/scala/computerdatabase/BasicItSimulation.scala
@@ -1,22 +1,22 @@
 package computerdatabase
 
+import com.github.phisgr.gatling.grpc.Predef._
 import io.gatling.core.Predef._
-import io.gatling.http.Predef._
+import io.grpc.ManagedChannelBuilder
+import io.grpc.health.v1.health.{HealthCheckRequest, HealthGrpc}
+
 import scala.concurrent.duration._
 
 class BasicItSimulation extends Simulation {
 
-  val httpProtocol = http
-    .baseUrl("http://computer-database.gatling.io") // Here is the root for all relative URLs
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
-    .acceptEncodingHeader("gzip, deflate")
-    .acceptLanguageHeader("en-US,en;q=0.5")
-    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
-
+  val grpcConf = grpc(ManagedChannelBuilder.forAddress("photoslibrary.googleapis.com", 443))
   val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(http("request_1")
-      .get("/"))
-    .pause(7) // Note that Gatling has recorder real time pauses
+    .exec(
+      grpc("request_1")
+        .rpc(HealthGrpc.METHOD_CHECK)
+        .payload(HealthCheckRequest.defaultInstance)
+    )
+    .pause(7.seconds)
 
-  setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
+  setUp(scn.inject(atOnceUsers(1)).protocols(grpcConf))
 }
